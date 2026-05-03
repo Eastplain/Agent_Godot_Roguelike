93d4054ce08001b52a24674e96aa2ed18b366c4a
Eastplain <53070419+Eastplain@users.noreply.github.com>
2026-05-03 21:13:52 +0800

    fix: ThemeDB全局字体 + canvas focus JS

--- export/web/index.service.worker.js
+++ export/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777813769|2855498';
+const CACHE_VERSION = '1777814030|2657228';
 /** @type {string} */
 const CACHE_PREFIX = 'Roguelike Demo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- scene/fontSetup.gd
+++ scene/fontSetup.gd
@@ -1,10 +1,8 @@
 extends Node
-# 启动时设置中文字体为全局默认
+# 启动时强制设中文字体
 
 func _ready():
 	var font = load("res://resource/font/simhei.ttf")
 	if font:
-		var theme = Theme.new()
-		theme.default_font = font
-		theme.default_font_size = 16
-		get_tree().root.theme = theme
+		ThemeDB.get_default_theme().default_font = font
+		ThemeDB.get_default_theme().default_font_size = 16
