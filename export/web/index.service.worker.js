e7baa475936a3682b7048bd9c820c6cdd64ae98c
Eastplain <53070419+Eastplain@users.noreply.github.com>
2026-05-03 21:09:38 +0800

    fix: Theme资源替代FontSetup + project.godot gui/theme/custom

--- export/web/index.service.worker.js
+++ export/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777812411|2475010';
+const CACHE_VERSION = '1777813769|2855498';
 /** @type {string} */
 const CACHE_PREFIX = 'Roguelike Demo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
